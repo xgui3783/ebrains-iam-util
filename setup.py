822c6ea4bbb0a36f7833547609693e080fcaa5ea
xgui3783 <xgui3783@gmail.com>
2023-08-03 15:57:36 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     author="Xiao Gui",
     author_email="xgui3783@gmail.com",
     description="Sync local directory to ebrains dataproxy",
-    packages=find_packages(include="ebrains_iam"),
+    packages=find_packages(include=["ebrains_iam"]),
     python_requires=">=3.7",
     install_requires=[
         "requests",
