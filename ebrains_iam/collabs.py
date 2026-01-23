3d8946b9be7d58c015d2d6b3856ee9918d8839ff
Xiao Gui <xgui3783@gmail.com>
2026-01-23 14:05:52 +0100

    feat: allow to iter collab

--- ebrains_iam/collabs.py
+++ ebrains_iam/collabs.py
@@ -8,7 +8,12 @@ from .config import wiki_endpoint
 from .common import camel_to_snake
 
 TYPE_ROLE = Literal["administrator", "editor", "viewer"]
-possible_roles = ("administrator", "editor", "viewer",)
+possible_roles = (
+    "administrator",
+    "editor",
+    "viewer",
+)
+
 
 @dataclass
 class Collab:
@@ -27,30 +32,31 @@ class Collab:
 
     @classmethod
     def from_json(cls, json_obj: dict):
-        return cls(**{
-            camel_to_snake(key): value
-            for key, value in json_obj.items()
-        })
-    
+        return cls(**{camel_to_snake(key): value for key, value in json_obj.items()})
+
     @property
     def base_url(self):
         return f"{wiki_endpoint}/rest/v1/collabs/{self.name}"
-    
+
     def list_teams(self, role: TYPE_ROLE, *, token: str):
         assert role in possible_roles, f"{role=!r} must be in {possible_roles}"
-        resp = requests.get(f"{self.base_url}/team/{role}", headers={
-            "Authorization": f"bearer {token}"
-        })
+        resp = requests.get(
+            f"{self.base_url}/team/{role}", headers={"Authorization": f"bearer {token}"}
+        )
         resp.raise_for_status()
         users = resp.json().get("users", [])
-        return {
-            "users": [User.from_json(user) for user in users]
-        }
-        
+        return {"users": [User.from_json(user) for user in users]}
 
-    def add_team(self, username: str, role: TYPE_ROLE, *, is_service_account: bool=False, token: str):
+    def add_team(
+        self,
+        username: str,
+        role: TYPE_ROLE,
+        *,
+        is_service_account: bool = False,
+        token: str,
+    ):
         """Adding a user to this collab with the specified role
-        
+
         Args:
             username: str username of the user. Can use OIDC client-id, but the is_service_account flag must be set to true
             role: str one of administrator, editor, viewer
@@ -61,15 +67,26 @@ class Collab:
         if is_service_account:
             username = f"service-account-{username}"
 
-        resp = requests.put(f"{self.base_url}/team/{role}/users/{username}", headers={
-            "Authorization": f"bearer {token}"
-        })
+        resp = requests.put(
+            f"{self.base_url}/team/{role}/users/{username}",
+            headers={"Authorization": f"bearer {token}"},
+        )
         resp.raise_for_status()
-        print(f"Adding user {username} to collab {self.name} as role {role} successful!", file=sys.stderr)
+        print(
+            f"Adding user {username} to collab {self.name} as role {role} successful!",
+            file=sys.stderr,
+        )
 
-    def remove_team(self, username: str, role: TYPE_ROLE, *, is_service_account: bool=False, token: str):
+    def remove_team(
+        self,
+        username: str,
+        role: TYPE_ROLE,
+        *,
+        is_service_account: bool = False,
+        token: str,
+    ):
         """Removing a user to this collab with the specified role
-        
+
         Args:
             username: str username of the user. Can use OIDC client-id, but the is_service_account flag must be set to true
             role: str one of administrator, editor, viewer
@@ -80,17 +97,37 @@ class Collab:
         if is_service_account:
             username = f"service-account-{username}"
 
-        resp = requests.delete(f"{self.base_url}/team/{role}/users/{username}", headers={
-            "Authorization": f"bearer {token}"
-        })
+        resp = requests.delete(
+            f"{self.base_url}/team/{role}/users/{username}",
+            headers={"Authorization": f"bearer {token}"},
+        )
         resp.raise_for_status()
-        print(f"Deleting user {username} from collab {self.name} as role {role} successful!", file=sys.stderr)
+        print(
+            f"Deleting user {username} from collab {self.name} as role {role} successful!",
+            file=sys.stderr,
+        )
 
 
 def get_collab(collab_id: str, *, token: str):
-    resp = requests.get(f"{wiki_endpoint}/rest/v1/collabs/{collab_id}", headers={
-        "Authorization": f"bearer {token}"
-    })
+    resp = requests.get(
+        f"{wiki_endpoint}/rest/v1/collabs/{collab_id}",
+        headers={"Authorization": f"bearer {token}"},
+    )
     resp.raise_for_status()
     return Collab.from_json(resp.json())
-    
+
+
+def finditer_collab(*, token: str):
+    limit = 50
+    offset = 0
+    while True:
+        resp = requests.get(
+            f"{wiki_endpoint}/rest/v1/collabs?limit={limit}&offset={offset}",
+            headers={"Authorization": f"bearer {token}"},
+        )
+        resp.raise_for_status()
+        data = resp.json()
+        if (len(data)) == 0:
+            break
+        yield from [Collab.from_json(datum) for datum in data]
+        offset += limit
